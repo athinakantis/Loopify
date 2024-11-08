50d407bc1c108cc7024973e9cda401374e906dca
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-08 14:47:08 +0200

    add accestoken, landingpage to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
 import './App.css';
 import SearchAndDisplay from './components/SearchAndDisplay/SearchAndDisplay.jsx';
+import LandingPage from './components/LandingPage/LandingPage.jsx';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <>
-      <header>
-        <img
-          id='logo'
-          src='src/assets/loopifyLogo_dark.svg'
-          alt='Loopify Logo'
-        />
-      </header>
-      <main>
-        <SearchAndDisplay placeholder='Search...' />
-      </main>
-    </>
-  );
+    const [accessToken, setAccessToken] = useState('');
+
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem('accessToken');
+
+        if (!token && hash) {
+            token = hash
+                .substring(1)
+                .split('&')
+                .find((el) => el.startsWith('access_token'))
+                .split('=')[1];
+
+            console.log('accesstoken: ', token);
+
+            window.location.hash = '';
+            setAccessToken(token);
+            localStorage.setItem('accessToken', token);
+        } else if (token) {
+            setAccessToken(token);
+        }
+    });
+
+    return (
+        <>
+            <header>
+                <img
+                    id='logo'
+                    src='src/assets/loopifyLogo_dark.svg'
+                    alt='Loopify Logo'
+                />
+            </header>
+            <main>
+                {!accessToken && <LandingPage />}
+                <SearchAndDisplay placeholder='Search...' />
+            </main>
+        </>
+    );
 }
 
 export default App;
