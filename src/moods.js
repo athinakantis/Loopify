83f7c142c332f9e4b2cea0c0fcda330c358b5b06
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-08 14:33:35 +0200

    add moods

---
+++ src/moods.js
@@ -0,0 +1,16 @@
+const moods = [
+    'high-energy',
+    'uplifted',
+    'inspired',
+    'mellow',
+    'peaceful',
+    'melancholic',
+    'angsty',
+    'joyful',
+    'winter',
+    'playful',
+    'calm',
+    'nostalgic',
+];
+
+export default moods;
