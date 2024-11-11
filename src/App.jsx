6177c57755a22b52667acdcf28c9c79fb338de8d
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-11 18:46:44 +0200

    remove suspense and add documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import './App.css';
 import SearchAndDisplay from './components/SearchAndDisplay/SearchAndDisplay.jsx';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../src/components/Header/Header.jsx';
 
 function App() {
+    // using state to manage accesstoken, and set when page is finished loading
     const [accessToken, setAccessToken] = useState('');
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        // Check if there is a token in localstorage and what the expiration of the token is.
         let token = window.localStorage.getItem('accessToken');
         let expiration = window.localStorage.getItem('expiration');
+        const currentTime = new Date();
+
+        // When login is approved by user, the accesstoken can be found in the hash.
         const hash = window.location.hash;
 
-        const currentTime = new Date()
-
+        // If currenttime is more than the expiration of the accesstoken, remove both these items
         if (currentTime > expiration) {
-            localStorage.removeItem('accessToken')
-            localStorage.removeItem('expiration')
-            setAccessToken('')
-        };
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('expiration');
+            setAccessToken('');
+        }
 
+        // If there is still a token, set accesstoken
         if (token) {
-            setAccessToken(token)
+            setAccessToken(token);
         } else if (!token && hash) {
+            // else if there is no token but there is a hash, create the token from the hash.
             token = hash
                 .substring(1)
                 .split('&')
@@ -33,23 +39,25 @@ function App() {
             window.location.hash = '';
             setAccessToken(token);
             localStorage.setItem('accessToken', token);
-            localStorage.setItem('expiration', currentTime.setHours(currentTime.getHours() + 1))
+            localStorage.setItem(
+                'expiration',
+                currentTime.setHours(currentTime.getHours() + 1)
+            );
         }
 
-        setLoading(false);
+        //Once the token is set or removed, set loading to false. 
+        setLoading(false); 
     }, []);
 
-    if (loading) return <p>Loading...</p>
+    if (loading) return <p>Loading...</p>;
 
     return (
         <>
-            <Suspense fallback={<p>Loading...</p>}>
-                <Header />
-                <main>
-                    {!accessToken && <LandingPage />}
-                    <SearchAndDisplay placeholder='Search...' />
-                </main>
-            </Suspense>
+            <Header />
+            <main>
+                {!accessToken && <LandingPage />}
+                <SearchAndDisplay accesstoken={accessToken} placeholder='Search...' />
+            </main>
         </>
     );
 }
