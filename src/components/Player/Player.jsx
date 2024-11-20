b739e3430252eb31eb1880ff4bebb1fd94d0428a
Athina <athina.kantis@gmail.com>
2024-11-20 14:40:43 +0200

    add player component

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,72 +1,24 @@
-import { useEffect, useState } from 'react';
-import AlbumCard from '../AlbumCard/AlbumCard'
 import './Player.css';
 
-function Player({ isOpen, setAccessToken, accessToken, playItem, setIsPlaying, isPlaying }) {
-    const [deviceID, setDeviceID] = useState('');
-    console.log(playItem)
-
+function Player({ isOpen, player, playItem, setIsPlaying, isPlaying }) {
+    // Toggle player
     function handlePlaying() {
         setIsPlaying((prev) => !prev);
+        player.togglePlay()
     }
 
-    useEffect(() => {
-        fetch(`https://api.spotify.com/v1/me/player/devices`, {
-            headers: { Authorization: `Bearer ${accessToken}` },
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                setDeviceID(data.devices?.[0]?.id);
-            });
-
-        [accessToken];
-    });
-
-    useEffect(() => {
-        const { uri, isTrack } = playItem;
-        let body;
-        isTrack ? body = JSON.stringify({ uris: [uri] }) : body = JSON.stringify({ context_uri: uri })
-        if (isPlaying) {
-            fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceID}`, {
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                    'Content-Type': 'application/json',
-                },
-                body: body,
-            })
-                .catch((error) => {
-                    if (error.status === 401) {
-                        localStorage.clear();
-                        setAccessToken('')
-                    }
-                })
-        } else if (!isPlaying) {
-            fetch(`https://api.spotify.com/v1/me/player/pause?device_id=${deviceID}`, {
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-            })
-                .catch((error) => {
-                    if (error.status === 401) {
-                        localStorage.clear();
-                        setAccessToken('')
-                    }
-                })
-        }
-    }, [isPlaying, playItem]);
-
     return (
-        <div className='player'>
+        <div id='player'>
+            {playItem &&
             <div className='playerCard'>
                 <p>{playItem.title}</p>
                 <p>{playItem.artist}</p>
                 <img src={playItem.img} alt={playItem.title} />
             </div>
+            }
 
             <div className={isOpen ? 'Expanded' : 'Collapsed'} id='playerControls'>
-                <button id='skipPrev'>
+                <button id='skipPrev' onClick={() => player.previousTrack()}>
                     <img
                         src='src/assets/player_skipPrevious.svg'
                         alt='Skip to previous song'
@@ -85,7 +37,7 @@ function Player({ isOpen, setAccessToken, accessToken, playItem, setIsPlaying, i
                         />
                     )}
                 </button>
-                <button id='skipNext'>
+                <button id='skipNext' onClick={() => player.nextTrack()}>
                     <img
                         src='src/assets/player_skipNext.svg'
                         alt='Skip to next song'
