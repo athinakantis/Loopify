c0debf2b41bd51637dd417d8ea5c13dfa7c508de
Athina <athina.kantis@gmail.com>
2024-11-22 12:36:30 +0200

    add handler functions

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,46 +1,55 @@
 import "./Player.css";
 import { useState } from "react";
 
-function Player({ isOpen, player, playItem, setIsPlaying, isPlaying }) {
+function Player({ isOpen, player, setIsPlaying, displayItem, isPlaying }) {
   const [volume, setVolume] = useState(0.3);
-
-  // Toggle player
-  function handlePlaying() {
-    player.togglePlay();
-    setIsPlaying((prev) => !prev);
-  }
 
   function handleVolume(e) {
     setVolume(e.target.value);
     player.setVolume(e.target.value);
   }
 
+  function handlePlay() {
+    setIsPlaying((prev) => !prev);
+    player.togglePlay();
+  }
+
+  function handleSkipNext() {
+    player.nextTrack();
+    setIsPlaying(true);
+  }
+
+  function handleSkipPrevious() {
+    player.previousTrack();
+    setIsPlaying(true);
+  }
+
   return (
     <div id="player">
-      {isOpen && playItem && (
+      {isOpen && displayItem && (
         <div className="playerCard">
-          <p>{playItem.name}</p>
-          <p>{playItem.artist}</p>
-          <img src={playItem.img} alt={playItem.name} />
+          <p>{displayItem.name}</p>
+          <p>{displayItem.artist}</p>
+          <img src={displayItem.img} alt={displayItem.name} />
         </div>
       )}
 
       <div className={isOpen ? "Expanded" : "Collapsed"} id="playerControls">
         <div className="playPause">
-          <button id="skipPrev" onClick={() => player.previousTrack()}>
+          <button id="skipPrev" onClick={handleSkipPrevious}>
             <img
               src="src/assets/player_skipPrevious.svg"
               alt="Skip to previous song"
             />
           </button>
-          <button id="playPause" onClick={handlePlaying}>
+          <button id="playPause" onClick={handlePlay}>
             {isPlaying ? (
               <img src="src/assets/player_pauseIcon.svg" alt="Pause Icon" />
             ) : (
               <img src="src/assets/player_playIcon.svg" alt="Play Icon" />
             )}
           </button>
-          <button id="skipNext" onClick={() => player.nextTrack()}>
+          <button id="skipNext" onClick={handleSkipNext}>
             <img src="src/assets/player_skipNext.svg" alt="Skip to next song" />
           </button>
         </div>
