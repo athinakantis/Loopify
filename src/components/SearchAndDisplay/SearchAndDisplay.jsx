1dc4f122e7c577811d0173f128da18f71072d5e6
Athina <athina.kantis@gmail.com>
2024-11-24 17:01:23 +0200

    fix player not playing issue

--- src/components/SearchAndDisplay/SearchAndDisplay.jsx
+++ src/components/SearchAndDisplay/SearchAndDisplay.jsx
@@ -1,173 +1,195 @@
-import clsx from "clsx";
-import { useState, useEffect, useCallback } from "react";
-import useSpotifyToken from "../useSpotifyToken/useSpotifyToken.jsx";
-import SongCard from "../SongCard/SongCard.jsx";
-import "./SearchAndDisplay.css";
-import "./SearchBar.css";
+import clsx from 'clsx';
+import { useState, useEffect } from 'react';
+import useSpotifyToken from '../useSpotifyToken/useSpotifyToken.jsx';
+import SongCard from '../SongCard/SongCard.jsx';
+import './SearchAndDisplay.css';
+import './SearchBar.css';
 
 export default function SearchAndDisplay(props) {
-  const {
-    type,
-    placeholder,
-    name,
-    className,
-    setPlayItem,
-    setIsPlaying,
-    ...rest
-  } = props;
-  const classes = clsx(className);
+    const {
+        type,
+        placeholder,
+        name,
+        className,
+        setPlayItem,
+        setIsPlaying,
+        ...rest
+    } = props;
+    const classes = clsx(className);
 
-  const accessToken = useSpotifyToken();
-  const [searchTerm, setSearchTerm] = useState("");
-  const [tracks, setTracks] = useState([]);
-  const [albums, setAlbums] = useState([]);
-  const [playlists, setPlaylists] = useState([]);
+    const accessToken = useSpotifyToken();
+    const [searchTerm, setSearchTerm] = useState('');
+    const [tracks, setTracks] = useState([]);
+    const [albums, setAlbums] = useState([]);
+    const [playlists, setPlaylists] = useState([]);
 
-  function handlePlay(uri, type = "track") {
-    setPlayItem({
-      uri: uri,
-      type: type,
-    });
-    setIsPlaying(true);
-  }
-
-  useEffect(() => {
-    if (searchTerm) {
-      fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          searchTerm
-        )}&type=track&limit=50`,
-        { headers: { Authorization: `Bearer ${accessToken}` } }
-      )
-        .then((response) => response.json())
-        .then((data) => setTracks(data.tracks.items))
-        .catch((error) => console.error("Error fetching tracks:", error));
-
-      fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          searchTerm
-        )}&type=album&limit=15`,
-        { headers: { Authorization: `Bearer ${accessToken}` } }
-      )
-        .then((response) => response.json())
-        .then((data) => setAlbums(data.albums.items))
-        .catch((error) => console.error("Error fetching albums:", error));
-
-      fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-          searchTerm
-        )}&type=playlist&limit=15`,
-        { headers: { Authorization: `Bearer ${accessToken}` } }
-      )
-        .then((response) => response.json())
-        .then((data) => setAlbums(data.playlists.items))
-        .catch((error) => console.error("Error fetching playlists:", error));
+    function handlePlay(uri, type) {
+        setPlayItem({
+            uri: uri,
+            type: type,
+        });
+        setIsPlaying(true);
     }
-  }, [searchTerm, accessToken]);
 
-  function handleKeyUp(event) {
-    event.preventDefault();
-    const query = event.target.value;
+    useEffect(() => {
+        if (searchTerm) {
+            fetch(
+                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+                    searchTerm
+                )}&type=track&limit=50`,
+                { headers: { Authorization: `Bearer ${accessToken}` } }
+            )
+                .then((response) => response.json())
+                .then((data) => setTracks(data.tracks.items))
+                .catch((error) =>
+                    console.error('Error fetching tracks:', error)
+                );
 
-    setSearchTerm(query);
+            fetch(
+                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+                    searchTerm
+                )}&type=album&limit=15`,
+                { headers: { Authorization: `Bearer ${accessToken}` } }
+            )
+                .then((response) => response.json())
+                .then((data) => setAlbums(data.albums.items))
+                .catch((error) =>
+                    console.error('Error fetching albums:', error)
+                );
 
-    if (query.trim() === "") {
-      setTracks([]);
-      setAlbums([]);
-      setPlaylists([]);
-      return;
+            fetch(
+                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+                    searchTerm
+                )}&type=playlist&limit=15`,
+                { headers: { Authorization: `Bearer ${accessToken}` } }
+            )
+                .then((response) => response.json())
+                .then((data) => setAlbums(data.playlists.items))
+                .catch((error) =>
+                    console.error('Error fetching playlists:', error)
+                );
+        }
+    }, [searchTerm, accessToken]);
+
+    function handleKeyUp(event) {
+        event.preventDefault();
+        const query = event.target.value;
+
+        setSearchTerm(query);
+
+        if (query.trim() === '') {
+            setTracks([]);
+            setAlbums([]);
+            setPlaylists([]);
+            return;
+        }
     }
-  }
 
-  return (
-    <>
-      <div className="search-bar">
-        <input
-          onKeyUp={handleKeyUp}
-          className={classes}
-          type={type}
-          placeholder={placeholder}
-          name={name}
-          {...rest}
-        />
-        <a href="#">
-          <span class="material-symbols-outlined">search</span>
-        </a>
-      </div>
-
-      <div className="displaySongs">
-        <h2>Top songs</h2>
-
-        {tracks.length > 0 && (
-          <div className="top-songs">
-            {tracks.map((track) => (
-              <SongCard
-                setPlayItem={setPlayItem}
-                key={track?.id}
-                uri={track?.uri}
-                name={track?.name}
-                artist={track?.artists
-                  ?.map((artist) => artist?.name)
-                  ?.join(", ")}
-                img={track?.album?.images?.[0]?.url}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-
-      <div className="displayAlbums">
-        <h2>Albums</h2>
-        <div className="albums">
-          {albums.map((album) => (
-            <div className="album" key={album?.id}>
-              <div className="albumImg">
-                <div className="albumIcons">
-                  <span class="material-symbols-outlined">play_arrow</span>
-                  <span class="material-symbols-outlined">playlist_add</span>
-                </div>
-                <button
-                  onClick={() => {
-                    handlePlay(album?.uri, "album");
-                  }}
-                >
-                  <img
-                    src={album?.images?.[0]?.url}
-                    width="150px"
-                    height="150px"
-                  />
-                </button>
-              </div>
-              <div className="albumInfo">
-                <p className="albumName">{album?.name}</p>
-                <p>{album?.artists?.[0]?.name}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="displayPlaylists">
-        <h2>Playlists</h2>
-        <div className="playlists">
-          {playlists.map((playlist) => (
-            <div className="playlist" key={playlist?.id}>
-              <button
-                onClick={() => {
-                  handlePlay(playlist?.uri, "playlist");
-                }}
-              >
-                <img
-                  src={playlist?.images?.[0]?.url}
-                  width="150px"
-                  height="150px"
+    return (
+        <>
+            <div className="search-bar">
+                <input
+                    onKeyUp={handleKeyUp}
+                    className={classes}
+                    type={type}
+                    placeholder={placeholder}
+                    name={name}
+                    {...rest}
                 />
-              </button>
-              <p className="playlistName">{playlist?.name}</p>
+                <a href="#">
+                    <span class="material-symbols-outlined">search</span>
+                </a>
             </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+
+            <div className="displaySongs">
+                <h2>Top songs</h2>
+
+                {tracks.length > 0 && (
+                    <div className="top-songs">
+                        {tracks.map((track) => (
+                            <SongCard
+                                setIsPlaying={setIsPlaying}
+                                setPlayItem={setPlayItem}
+                                key={track?.id}
+                                uri={track?.uri}
+                                name={track?.name}
+                                artist={track?.artists
+                                    ?.map((artist) => artist?.name)
+                                    ?.join(', ')}
+                                img={track?.album?.images?.[0]?.url}
+                            />
+                        ))}
+                    </div>
+                )}
+            </div>
+
+            <div className="displayAlbums">
+                <h2>Albums</h2>
+                <div className="albums">
+                    {albums.map((album) => (
+                        <div
+                            className="album"
+                            key={album?.id}
+                        >
+                            <div className="albumImgContainer">
+                                <div className="albumIcons">
+                                    <button
+                                        onClick={() => {
+                                            handlePlay(album?.uri, 'album');
+                                        }}
+                                    >
+                                        <img
+                                            src="src/assets/play_arrow.svg"
+                                            alt="Play"
+                                        />
+                                    </button>
+                                    <button>
+                                        <img
+                                            src="src/assets/playlistAddIcon.svg"
+                                            alt="Add to playlist"
+                                        />
+                                    </button>
+                                </div>
+                                <img
+                                    className="albumImg"
+                                    src={album?.images?.[0]?.url}
+                                    width="150px"
+                                    height="150px"
+                                />
+                            </div>
+                            <div className="albumInfo">
+                                <p className="albumName">{album?.name}</p>
+                                <p>{album?.artists?.[0]?.name}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="displayPlaylists">
+                <h2>Playlists</h2>
+                <div className="playlists">
+                    {playlists.map((playlist) => (
+                        <div
+                            className="playlist"
+                            key={playlist?.id}
+                        >
+                            <button
+                                onClick={() => {
+                                    handlePlay(playlist?.uri, 'playlist');
+                                }}
+                            >
+                                <img
+                                    src={playlist?.images?.[0]?.url}
+                                    width="150px"
+                                    height="150px"
+                                />
+                            </button>
+                            <p className="playlistName">{playlist?.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
