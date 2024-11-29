5d9fa5ed189ae7d7897ce3921091a18e811c24c3
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-11-29 10:36:22 +0200

    update moods to be objects

--- src/moods.js
+++ src/moods.js
@@ -1,16 +1,196 @@
 const moods = [
-    'high-energy',
-    'uplifted',
-    'inspired',
-    'mellow',
-    'peaceful',
-    'melancholic',
-    'angsty',
-    'joyful',
-    'winter',
-    'playful',
-    'calm',
-    'nostalgic',
+    {
+        id: 0,
+        name: 'high-energy',
+        genres: [
+            'EDM',
+            'Techno',
+            'Drum and Bass',
+            'Hard Rock',
+            'Electro House',
+            'Punk',
+            'Trap',
+            'Dubstep',
+            'Industrial',
+            'Metalcore'
+        ]
+    },
+    {
+        id: 1,
+        name: 'uplifted',
+        genres: [
+            'Pop',
+            'Dance-Pop',
+            'Indie Pop',
+            'Funk',
+            'Tropical House',
+            'Synthwave',
+            'Afrobeat',
+            'Ska',
+            'Reggaeton',
+            'Pop Rock'
+        ]
+    },
+    {
+        id: 2,
+        name: 'inspired',
+        genres: [
+            'Alternative',
+            'Art Pop',
+            'Indie Folk',
+            'Post-Rock',
+            'Progressive Rock',
+            'Singer-Songwriter',
+            'Orchestral',
+            'Soul',
+            'World Music',
+            'Experimental'
+        ]
+    },
+    {
+        id: 3,
+        name: 'mellow',
+        genres: [
+            'Chillout',
+            'Lo-fi Hip Hop',
+            'Ambient',
+            'Jazz',
+            'Soft Rock',
+            'Acoustic',
+            'Dream Pop',
+            'Smooth Jazz',
+            'Downtempo',
+            'Folk'
+        ]
+    },
+    {
+        id: 4,
+        name: 'peaceful',
+        genres: [
+            'Meditation Music',
+            'Classical Piano',
+            'Nature Sounds',
+            'Bossa Nova',
+            'New Age',
+            'Celtic',
+            'Minimalism',
+            'Ethereal Wave',
+            'Acoustic Chill',
+            'Contemporary Instrumental'
+        ]
+    },
+    {
+        id: 5,
+        name: 'melancholic',
+        genres: [
+            'Blues',
+            'Darkwave',
+            'Slowcore',
+            'Emo',
+            'Post-Punk',
+            'Gothic Rock',
+            'Trip-Hop',
+            'Ambient Drone',
+            'Baroque Pop',
+            'Sad Indie'
+        ]
+    },
+    {
+        id: 6,
+        name: 'angsty',
+        genres: [
+            'Screamo',
+            'Grunge',
+            'Emo Punk',
+            'Hardcore Punk',
+            'Post-Hardcore',
+            'Nu Metal',
+            'Alternative Metal',
+            'Rap Metal',
+            'Noise Rock',
+            'Industrial Metal'
+        ]
+    },
+    {
+        id: 7,
+        name: 'joyful',
+        genres: [
+            'Bubblegum Pop',
+            'Disco',
+            'Ska Punk',
+            'Calypso',
+            'Soca',
+            'Happy Hardcore',
+            'Swing',
+            'Electropop',
+            'Gospel',
+            "Children's Music"
+        ]
+    },
+    {
+        id: 8,
+        name: 'winter',
+        genres: [
+            'Christmas',
+            'Neo-Classical',
+            'Dark Ambient',
+            'Post-Rock',
+            'Chamber Music',
+            'Nordic Folk',
+            'Piano Ballads',
+            'Shoegaze',
+            'Drone',
+            'Ethereal Folk'
+        ]
+    },
+    {
+        id: 9,
+        name: 'playful',
+        genres: [
+            'J-Pop',
+            'K-Pop',
+            'Chiptune',
+            'Electro Swing',
+            'Dancehall',
+            'Cumbia',
+            'Ukulele Pop',
+            'Cartoon Music',
+            'Hyperpop',
+            '8-bit'
+        ]
+    },
+    {
+        id: 10,
+        name: 'calm',
+        genres: [
+            'Ambient Chill',
+            'Zen Music',
+            'Chillhop',
+            'Classical Guitar',
+            'Gregorian Chant',
+            'Lullabies',
+            'Harp Music',
+            'Nature Sounds',
+            'Yoga Tunes',
+            'Minimalist Piano'
+        ]
+    },
+    {
+        id: 11,
+        name: 'nostalgic',
+        genres: [
+            'Retro Wave',
+            'Vintage Jazz',
+            'Oldies',
+            'Motown',
+            'Classic Rock',
+            '80s Synthpop',
+            '90s Grunge',
+            'Doo-Wop',
+            'Barbershop Quartet',
+            'Folk Revival'
+        ]
+    }
 ];
 
 export default moods;
