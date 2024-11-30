a756a6046b0d119450c02ee726dba6996066249f
Athina <athina.kantis@gmail.com>
2024-11-30 20:44:30 +0200

    seperate fetch requests from Component

--- src/components/SearchAndDisplay/SearchAndDisplay.jsx
+++ src/components/SearchAndDisplay/SearchAndDisplay.jsx
@@ -1,13 +1,12 @@
 import clsx from 'clsx';
 import { useState, useEffect } from 'react';
-import useSpotifyToken from '../useSpotifyToken/useSpotifyToken.jsx';
 import SongCard from '../SongCard/SongCard.jsx';
 import './SearchAndDisplay.css';
 import './SearchBar.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import moods from '../../moods.js';
-import { fetchMoods } from '../../utils/requests/moodFetchRequest.js';
+import { fetchMoods, fetchSearch } from '../../utils/requests/requests.js';
 import Spinner from '../Spinner/Spinner.jsx';
 
 export default function SearchAndDisplay(props) {
@@ -15,13 +14,13 @@ export default function SearchAndDisplay(props) {
         type,
         placeholder,
         name,
+        accessToken,
         className,
         setPlayItem,
         setIsPlaying,
         ...rest
     } = props;
     const classes = clsx(className);
-    const accessToken = useSpotifyToken();
     const [searchTerm, setSearchTerm] = useState('');
     const [tracks, setTracks] = useState([]);
     const [albums, setAlbums] = useState([]);
@@ -41,7 +40,7 @@ export default function SearchAndDisplay(props) {
         if (moodId === id) {
             setMoodId();
         } else {
-            setMoodId(id);
+            setMoodId(id.toString());
         }
     }
 
@@ -51,7 +50,7 @@ export default function SearchAndDisplay(props) {
             async function getMoodPlaylists() {
                 try {
                     setLoading(true);
-                    setPlaylists(await fetchMoods(moodId));
+                    setPlaylists(await fetchMoods(+moodId));
                     setLoading(false);
                 } catch (err) {
                     console.error(err);
@@ -65,51 +64,20 @@ export default function SearchAndDisplay(props) {
 
     useEffect(() => {
         if (searchTerm) {
-            fetch(
-                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-                    searchTerm
-                )}&type=track&limit=50`,
-                { headers: { Authorization: `Bearer ${accessToken}` } }
-            )
-                .then((response) => response.json())
-                .then((data) => {
-                    setTracks(
-                        data.tracks.items.filter((item) => item !== null)
-                    );
-                })
-                .catch((error) =>
-                    console.error('Error fetching tracks:', error)
-                );
-
-            fetch(
-                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-                    searchTerm
-                )}&type=album&limit=15`,
-                { headers: { Authorization: `Bearer ${accessToken}` } }
-            )
-                .then((response) => response.json())
-                .then((data) =>
-                    setAlbums(data.albums.items.filter((item) => item !== null))
-                )
-                .catch((error) =>
-                    console.error('Error fetching albums:', error)
-                );
-
-            fetch(
-                `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-                    searchTerm
-                )}&type=playlist&limit=15`,
-                { headers: { Authorization: `Bearer ${accessToken}` } }
-            )
-                .then((response) => response.json())
-                .then((data) =>
-                    setPlaylists(
-                        data.playlists.items.filter((item) => item !== null)
-                    )
-                )
-                .catch((error) =>
-                    console.error('Error fetching playlists:', error)
-                );
+            async function getSearchResults() {
+                try {
+                    setLoading(true);
+                    setMoodId();
+                    const results = await fetchSearch(searchTerm);
+                    setTracks(results.tracks);
+                    setAlbums(results.albums);
+                    setPlaylists(results.playlists);
+                    setLoading(false);
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+            getSearchResults();
         }
     }, [searchTerm, accessToken]);
 
@@ -148,9 +116,9 @@ export default function SearchAndDisplay(props) {
                 {moods.map((mood) => (
                     <button
                         className={
-                            moodId === mood.id ? 'currentMood' : 'moodButton'
+                            moodId == mood.id ? 'currentMood' : 'moodButton'
                         }
-                        onClick={() => handleMoodClick(mood.id)}
+                        onClick={() => handleMoodClick(mood?.id)}
                         key={mood?.id}
                     >
                         {mood?.name}
@@ -161,7 +129,9 @@ export default function SearchAndDisplay(props) {
             <div className='displaySongs'>
                 <h2>Top songs</h2>
 
-                {tracks.length > 0 && (
+                {loading ? (
+                    <Spinner />
+                ) : (
                     <div className='top-songs'>
                         {tracks.map((track) => (
                             <SongCard
