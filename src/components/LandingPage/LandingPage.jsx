a26cdff590031263e87c231b43961a442e2170ea
Athina <athina.kantis@gmail.com>
2024-12-08 16:12:28 +0200

    update scope

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -8,7 +8,17 @@ function LandingPage() {
     const clientID = '31b7c8cccb774c259278744de0c20018';
     const redirectURI = 'http://localhost:5173/'; // Update URI after deployment
     const scopes = [
-        'playlist-read-private', 'playlist-read-collaborative', 'playlist-modify-public', 'playlist-modify-private', 'user-modify-playback-state', 'user-read-playback-state', 'streaming', 'user-read-email', 'user-read-private'
+        'playlist-read-private',
+        'playlist-read-collaborative',
+        'playlist-modify-public',
+        'playlist-modify-private',
+        'user-modify-playback-state',
+        'user-read-playback-state',
+        'streaming',
+        'user-read-email',
+        'user-read-private',
+        'user-library-read',
+        'user-top-read',
     ];
 
     // Updates the mood every 1000ms, resets the mood if it's reached the full length of moods array
@@ -33,7 +43,9 @@ function LandingPage() {
                 </p>
 
                 <a
-                    href={`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=${scopes.join('+')}`}
+                    href={`https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&response_type=token&scope=${scopes.join(
+                        '+'
+                    )}`}
                 >
                     <button>Get Started</button>
                 </a>
